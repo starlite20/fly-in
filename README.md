701c24fe9f8d9a276472bfd2aa9f58ef3202beab
Suhail <suhail542@gmail.com>
2026-05-10 19:05:16 +0400

    working on zone class

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fly-in
+
+
+# resources
+
+https://www.freecodecamp.org/news/i-dont-understand-graph-theory-1c96572a1401/
+https://www.freecodecamp.org/news/the-value-of-graph-theory-within-sustainability/
+https://www.youtube.com/watch?v=09_LlHjoEiY
--- code/graphs.py
+++ code/graphs.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
-class Colors(Enum):
-    GREY = "grey"
-    BLUE = "blue"
-    RED = "red"
-    YELLOW = "yellow"
-    GREEN = "green"
+
+class Zonerole(Enum):
+    START = "start_hub"
+    END = "end_hub"
+    HUB = "hub"
+
 
 class Zonetype(Enum):
     NORMAL = "normal"
@@ -17,27 +17,20 @@ class Zonetype(Enum):
 class Zone():
     # sample zone data
     # (['start', 0, 0], {'color': 'green', 'max_drones': '5'})
-    def __init__(self, zone_data: tuple[list, dict]):
-        self.is_start = False
-        self.is_end = False
+    def __init__(self, role: Zonerole, zone_data: tuple[list, dict]):
+        self.role = role
+        self.is_start = (role == Zonerole.START)
+        self.is_end = (role == Zonerole.END)
 
         essential, zone_metadata = zone_data
 
-        self.set_hubname(essential[0])
-        self.set_zone_coordinates(essential[1], essential[2])
+        self.hubname = essential[0]
+        self.zone_coordinate = (essential[1], essential[2])
         self.set_metadata(zone_metadata)
 
     def __str__(self):
         str_val = f"{self.hubname.capitalize()} Zone '{self.hubname}'\n"
-
-        if self.is_start:
-            str_val += "Start Hub"
-        elif self.is_end:
-            str_val += "End Hub"
-        else:
-            str_val += "Regular Hub"
-
-        str_val += f" at {self.zone_coordinate}\n"
+        str_val += f"{self.role} at {self.zone_coordinate}\n"
         str_val += "Metadata\n"
         str_val += f"-- Color : {self.color}\n"
         str_val += f"-- Max Drones : {self.max_drones}\n"
@@ -45,29 +38,9 @@ class Zone():
 
         return str_val
 
-    def set_hubname(self, hubname: str):
-        self.hubname = hubname
-        if hubname == "start":
-            self.is_start = True
-        elif hubname == "end":
-            self.is_end = True
-
-    def set_zone_coordinates(self, x_val: int, y_val: int):
-        self.zone_coordinate = (x_val, y_val)
-    
     def set_metadata(self, metadata):
-        if not metadata:
-            return
-        
         # color metadata
-        color_str = metadata.get("color")
-        if color_str:
-            try:
-                self.color = Colors(color_str.lower())
-            except ValueError:
-                raise ValueError(f"Invalid Color Value Passed -> '{color_str}'")
-        else:
-            self.color = Colors.GREY
+        self.color = metadata.get("color", "none")
 
         # zonetype metadata
         zone_type_str = metadata.get("zone")
@@ -75,7 +48,8 @@ class Zone():
             try:
                 self.zone_type = Zonetype(zone_type_str.lower())
             except ValueError:
-                raise ValueError(f"Invalid Zone Type Passed -> '{zone_type_str}'")
+                raise ValueError(
+                    f"Invalid Zone Type Passed -> '{zone_type_str}'")
         else:
             self.zone_type = Zonetype.NORMAL
 
@@ -85,7 +59,8 @@ class Zone():
             try:
                 self.max_drones = int(max_drones_str)
             except ValueError:
-                raise ValueError(f"Invalid Value passed of Max Drones -> '{max_drones_str}'")
+                raise ValueError(
+                    f"Invalid Value passed of Max Drones -> '{max_drones_str}'")
         else:
             self.max_drones = 1
 
@@ -98,6 +73,3 @@ class Zone():
         #         raise ValueError(f"Invalid Value passed of Max Link Capacity -> '{max_drones_str}'")
         # else:
         #     self.max_link_capacity = 1
-
-    
-
